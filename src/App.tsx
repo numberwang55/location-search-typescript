2525a39c8e8cf1daed78e669efd9a960faa76bd5
numberwang55 <cban0901@hotmail.com>
2024-05-10 10:20:46 +0100

    Add two new components to project with skeleton rendering: [Map] and [LocationSearch]

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import Map from "./components/Map";
+import LocationSearch from "./components/LocationSearch";
+
 function App() {
-  return <div className="m-4">Hello</div>;
+  return (
+    <div className="h-screen w-screen grid grid-cols-12">
+      <div className="col-span-3 p-2">
+        <LocationSearch />
+      </div>
+      <div className="col-span-9">
+        <Map />
+      </div>
+    </div>
+  );
 }
 
 export default App;
